0954fd223a172f91d22b7cfb60351d241238ba17
= <=>
2015-07-26 13:56:39 -0500

    Correcion de asingnacion de productos a empaque

--- empaque/asignar/agregar_producto_empaque.php
+++ empaque/asignar/agregar_producto_empaque.php
@@ -3,8 +3,10 @@
 
 		$idProducto	=	$_POST['idProducto'];
 		$idEmpaque	=	$_POST['idEmpaque'];
+		
+//		$result = mysql_query("select id_producto_fk from productos_empaques join usuario_empaque on productos_empaques.id_empaque_fk = usuario_empaque.id_empaque_fk join productos on productos.id_producto = productos_empaques.id_producto_fk where usuario_empaque.id_usuario_fk =".$_SESSION['id_usuario']);
 
-		$result = mysql_query("select id_producto_fk from productos_empaques join usuario_empaque on productos_empaques.id_empaque_fk = usuario_empaque.id_empaque_fk join productos on productos.id_producto = productos_empaques.id_producto_fk where usuario_empaque.id_usuario_fk =".$_SESSION['id_usuario']);
+		$result = "select id_producto_fk from productos_empaques where id_empaque_fk = $_SESSION[id_empaque]";
 		$bandera=0;
 		if(mysql_num_rows($result) > 0){
 			while($row = mysql_fetch_array($result)){
@@ -15,7 +17,8 @@
 		}
 
 		if($bandera == 0){
-			mysql_query("INSERT INTO productos_empaques(id_empaque_fk, id_producto_fk, precio_producto) VALUES ($idEmpaque, $idProducto,0)");
+			$query="INSERT INTO productos_empaques(id_empaque_fk, id_producto_fk, precio_venta, precio_compra) VALUES ($idEmpaque, $idProducto,0,0)";
+			mysql_query($query);
 		}else
 			echo "e";
 		mysql_close();
