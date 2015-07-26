5994433493cff37a6370133c477a48a6e9a45ba4
Alfonso Calderón <alfonso.calderon.chavez@gmail.com>
2015-07-26 15:41:38 -0500

    sub

--- webservice/registroEnvios.php
+++ webservice/registroEnvios.php
@@ -24,7 +24,7 @@
 				}
 			}
 
-			if($tipo==2){//Registrar el envio 
+			if($tipo==2){//Registrar el del empaque
 				$id_dist = $datos[4];
 				$id_usuario = $datos[5];
 
@@ -54,6 +54,43 @@
 
 		break;
 		case 3://distribuidor
+			if($tipo==1){//Obtener datos del punto de venta
+				$query = "SELECT id_punto_venta, nombre_punto_venta FROM ordenes_punto_venta AS opv, usuario_punto_venta AS upv, empresa_punto_venta AS epv WHERE opv.id_orden = $orden AND opv.id_usuario_punto_venta_fk = upv.id_usuario_pv AND upv.id_punto_venta_fk = epv.id_punto_venta";
+				$resultado = mysql_query($query);
+				if(mysql_num_rows($resultado) > 0){
+					$row = mysql_fetch_array($resultado);
+					$datos_usuario = $row['id_punto_venta'].",".$row['nombre_punto_venta'];
+				}
+			}
+
+			if($tipo==2){//Registrar el envio del distribuidor
+				$id_pv= $datos[4];
+				$id_usuario = $datos[5];
+
+				$query = "INSERT INTO envios_distribuidor(fecha_envio, hora_envio, fecha_entrega_envio, id_camion_fk, id_usuario_distribuidor_fk, descripcion_envio, estado_envio, id_punto_venta_fk, id_orden_dist_fk) VALUES ('".date("Y-m-d")."', '".date("H:i:s")."', '0000-00-00', $carro, $id_usuario, 'descripcion', 7, $id_pv, $orden)";
+				$r = mysql_query($query);
+
+				if($r){
+					$datos_usuario = "Envio registrado como pendiente. \n -Proceda a leer todas las cajas y tarimas para completar el envio.";
+					mysql_query("UPDATE camiones_distribuidor set estado = 1 where id_camion = $carro");
+				}
+
+
+			}
+
+			if($tipo == 3){//Informacion para llenar la tabla de preenvios de la handaheld
+				$id_usuario = $datos[4];
+				$query = "SELECT id_envio, id_camion_fk, id_orden_fk FROM envios_distribuidor AS ed WHERE ed.id_usuario_distribuidor_fk = $id_usuario AND estado_envio = 7";
+				$r = mysql_query($query);
+				if(mysql_num_rows($r) > 0){
+					$datos_usuario = "Bien*";
+					while($row=mysql_fetch_array($r)){
+						$datos_usuario .= $row['id_envio'].",".$row['id_camion_fk'].",".$row['id_orden_fk'].",";
+					}
+				}else
+					$datos_usuario = "Error*Error";
+			}
+
 		break;
 		case 4://punto venta
 		break;
