acc9b23b77a30b3035d3f7b0c30263f2821b8732
Alfonso Calderón <alfonso.calderon.chavez@gmail.com>
2015-07-28 10:18:08 -0500

    a

---
+++ Trazabilidad_HH/Source/CS101 Callback Demo/entradasWorgking.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace CS101_CALLBACK_API_DEMO
+{
+    partial class entradasWorgking
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.dataGrid1 = new System.Windows.Forms.DataGrid();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.empaque_lbl = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dataGrid1
+            // 
+            this.dataGrid1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.dataGrid1.Location = new System.Drawing.Point(4, 24);
+            this.dataGrid1.Name = "dataGrid1";
+            this.dataGrid1.Size = new System.Drawing.Size(313, 103);
+            this.dataGrid1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(0, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(320, 20);
+            this.label1.Text = "Envios recibidos:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(4, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 20);
+            this.label2.Text = "De:";
+            // 
+            // empaque_lbl
+            // 
+            this.empaque_lbl.Location = new System.Drawing.Point(46, 133);
+            this.empaque_lbl.Name = "empaque_lbl";
+            this.empaque_lbl.Size = new System.Drawing.Size(271, 20);
+            this.empaque_lbl.Text = "empaque_lbl";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(4, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 25);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Continuar";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(181, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 25);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Finalizar entrega";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(244, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(72, 20);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Atras";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // entradasWorgking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(320, 240);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.empaque_lbl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGrid1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "entradasWorgking";
+            this.Text = "entradasWorgking";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGrid dataGrid1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label empaque_lbl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ Trazabilidad_HH/Source/CS101 Callback Demo/entradasWorgking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CS101_CALLBACK_API_DEMO
+{
+    public partial class entradasWorgking : Form
+    {
+        public entradasWorgking()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
