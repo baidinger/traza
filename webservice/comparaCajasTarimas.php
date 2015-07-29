7fb360739a32c67de688641830cc7221e654e867
Alfonso Calderón <alfonso.calderon.chavez@gmail.com>
2015-07-29 11:54:17 -0500

    a

--- webservice/comparaCajasTarimas.php
+++ webservice/comparaCajasTarimas.php
@@ -25,28 +25,38 @@
 				$id_envio = $row['id_envio'];
 
 
-				$consulta = "SELECT * FROM distribuidor_cajas_envio WHERE id_envio_fk = $id_envio AND epc_tarima = '$tarima'";
+				$consulta = "SELECT * FROM distribuidor_cajas_envio WHERE epc_tarima = '$tarima'";
 				$r = mysql_query($consulta);
 
 				if(mysql_num_rows($r) > 0){
-					for($i = 0; $i < count($epcCajas); $i++){
-						$consulta = "SELECT * FROM distribuidor_cajas_envio WHERE epc_caja = '".$epcCajas[$i]."' AND id_envio_fk = $id_envio AND epc_tarima = '$tarima'";
-						$resultado = mysql_query($consulta);
 
-						if(mysql_num_rows($resultado) > 0)
-							$consulta = "UPDATE distribuidor_cajas_envio SET recibido_dce = 1 WHERE epc_caja = '".$epcCajas[$i]."' AND id_envio_fk = $id_envio AND epc_tarima = '$tarima'";
-						else
-							$consulta = "INSERT INTO distribuidor_cajas_envio(id_envio_fk, epc_caja, epc_tarima, enviado_dce, recibido_dce) VALUES($id_envio, '".$epcCajas[$i]."', '$tarima', 0, 1)";
+					$consulta = "SELECT * FROM distribuidor_cajas_envio WHERE id_envio_fk = $id_envio AND epc_tarima = '$tarima'";
+					$r = mysql_query($consulta);
+				
 
-						mysql_query($consulta);
-					}	
+					if(mysql_num_rows($r) > 0){
+						for($i = 0; $i < count($epcCajas); $i++){
+							$consulta = "SELECT * FROM distribuidor_cajas_envio WHERE epc_caja = '".$epcCajas[$i]."' AND id_envio_fk = $id_envio AND epc_tarima = '$tarima'";
+							$resultado = mysql_query($consulta);
+
+							if(mysql_num_rows($resultado) > 0)
+								$consulta = "UPDATE distribuidor_cajas_envio SET recibido_dce = 1 WHERE epc_caja = '".$epcCajas[$i]."' AND id_envio_fk = $id_envio AND epc_tarima = '$tarima'";
+							else
+								$consulta = "INSERT INTO distribuidor_cajas_envio(id_envio_fk, epc_caja, epc_tarima, enviado_dce, recibido_dce) VALUES($id_envio, '".$epcCajas[$i]."', '$tarima', 0, 1)";
+
+							mysql_query($consulta);
+						}	
 
 
-					$datos_usuario = "Bien*Registro Exitoso";
-				}else
-					$datos_usuario = "Error*La tarima no pertenece a este envio";
+						$datos_usuario = "Bien*Registro Exitoso";
+					}else
+						$datos_usuario = "Error*La tarima no pertenece a este envio";
+				}else{
+
+					$datos_usuario = "Bien*La tarima NO esta registrada como ENVIADA en ningún envio. \n -Se registró como NO ENVIADA pero SI RECIBIDA.";
+				}			
 			}else
-				$datos_usuario = "Error*Error";
+					$datos_usuario = "Error*Error";
 
 		break;
 		case 4://punto venta
