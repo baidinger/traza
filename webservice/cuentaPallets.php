cbc1cfd3869f5f935daa7358b73ca4b65b3552be
Alfonso Calderón <alfonso.calderon.chavez@gmail.com>
2015-07-29 01:43:24 -0500

    a

--- webservice/cuentaPallets.php
+++ webservice/cuentaPallets.php
@@ -47,7 +47,8 @@
 			}
 
 			if($tipo == 3){
-				$query = "SELECT count(epc_tarima) FROM distribuidor_cajas_envio WHERE id_envio_fk = $id_envio AND enviado_dce = 1 GROUP BY epc_tarima";
+				$pallet = $datos[3];
+				$query = "SELECT count(epc_cajas) FROM distribuidor_cajas_envio WHERE id_envio_fk = $id_envio AND enviado_dce = 1 AND epc_tarima = $pallet GROUP BY epc_tarima";
 
 				$result = mysql_query($query);
 				if($result){
